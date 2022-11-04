d25ce83f579bd376ac90b6793333c74a2f790cad
Gotam Gorabh <gautamy672@gmail.com>
2022-11-04 18:02:35 +0530

    correction

--- ImageFormater/home.go
+++ ImageFormater/home.go
@@ -66,83 +66,34 @@ func (me *home)style() string {
 
 func (me *home)layout()string{
 
-
-return`
-
-{
-
-import: {
-
-    ui: nuxui.org/nuxui/ui,
-
-}
-
-layout: {
-
-    type: ui.Column,
-    width: 100%,
-    height: 100%,
-    background: #00ff0033,
-    children: [
-
+    return `
     {
-       type: ui.Column,
-       width: 100%,
-       height: 50%,
-       background: #ff000033,
-    },{
-
-       type: ui.Column,
-       width: 100%,
-       height: 50%,
-       background: #0000ff33,
-       children: [
-       {
-          type: ui.Row,
-          width: 100%,
-          children: [
-
-          {
-             type: ui.Text,
-             text: "Convert To:",
-
-
-          },{
-
-                type: ui.Options,
-                single: true,
-                content: {
-
-                      type: ui.Row,
-                      children: [
-
-                          {style:[radio], text:bmp },
-                          {style:[radio], text:ico },
-
-                      ],
-
-                },
-
-
+          import: {
+              ui: nuxui.org/nuxui/ui,
           }
 
-          ],
-
-
-       }
-
-       ],
+          layout: {
+              type: ui.Column,
+              width: 100%,
+              height: 100%,
+              background: #00ff0033,
+              children: [
+                  {
+                      type: ui.Column,
+                      width: 100%,
+                      height: 50%,
+                      background: #ff000033,
+                  },{
+                      type: ui.Column,
+                      width: 100%,
+                      height: 50%,
+                      background: #0000ff33,
+                  }
+              ],
+          }
 
     }
 
-    ],
-
-
-}
-
-}
-
-
-`
+    `
 
 }
