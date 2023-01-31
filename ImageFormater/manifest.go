1c2af8f106cbe9510515716f08854f6f743d05f7
Gotam Gorabh <gautamy672@gmail.com>
2023-01-31 22:09:38 +0530

    refactoring the manifest.go file

--- ImageFormater/manifest.go
+++ ImageFormater/manifest.go
@@ -3,33 +3,32 @@ package main
 // Do not change this name, nux need manifest to generate AndroidManifest.xml
 const manifest = `
 {
-    import: {
-        ui: "nuxui.org/nuxui/ui",
-    },
+  import: {
+    ui: "nuxui.org/nuxui/ui",
+  },
 
-    application: {
-        // display name at luancher
+  application: {
+    // display name at luancher
 		label: hello,
 
-        // application identifier name
-        name: "org.nuxui.samples.ImageFormater",
-    },
+    // application identifier name
+    name: "org.nuxui.samples.ImageFormater",
+  },
 
-    permissions: [
-        // wifi,
-        storage,
-        viewPhoto,
-        savePhoto,
-    ],
+  permissions: [
+    storage,
+    viewPhoto,
+    savePhoto,
+  ],
 
-    mainWindow: {
-        width: 750px,
-        height: 1:1,
-        title: "Image Foramter",
-        content: {
-            type: main.Home,
-        },
-        background: #ffffff,
+  mainWindow: {
+    width: 750px,
+    height: 1:1,
+    title: "Image Foramter",
+    content: {
+      type: main.Home,
     },
+    background: #ffffff,
+  },
 }
 `
